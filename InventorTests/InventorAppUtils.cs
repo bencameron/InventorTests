c79bd1f83ad8507a3c01e8b462ad2248f9cdb0a2
Ben Cameron <ben.cameron@d3automation.net>
2026-03-06 15:30:33 -0600

    Initial commit

---
+++ InventorTests/InventorAppUtils.cs
@@ -0,0 +1,54 @@
+﻿using Inventor;
+using System.Runtime.InteropServices;
+
+namespace InventorTests
+{
+    public static class InventorAppUtils
+    {
+        [DllImport("oleaut32.dll", PreserveSig = false)]
+        static extern void GetActiveObject(ref Guid rclsid, IntPtr pvReserved,
+                                            [MarshalAs(UnmanagedType.IUnknown)] out object ppunk);
+
+        [DllImport("ole32.dll")]
+        static extern int CLSIDFromProgID(
+            [MarshalAs(UnmanagedType.LPWStr)] string lpszProgID, out Guid pclsid);
+
+        /// <summary>
+        /// Gets the currently-running instance of Inventor or starts a new one if one is not already running
+        /// </summary>
+        /// <returns></returns>
+        /// <remarks></remarks>
+        public static Application GetInventorInstance(bool forceNewInstance = false)
+        {
+            Application app = null;
+
+            if (!forceNewInstance)
+            {
+                //Check for an instance of inventor, if one is not found then create one.
+                try
+                {
+                    CLSIDFromProgID("Inventor.Application", out var classId);
+                    GetActiveObject(ref classId, default, out var appObject);
+                    app = (Application)appObject;
+                }
+                catch (Exception)
+                {
+                    app = GetNewInventorInstance();
+                }
+            }
+
+            if (app == null)
+            {
+                app = GetNewInventorInstance();
+            }
+
+            return app;
+        }
+
+        private static Application GetNewInventorInstance()
+        {
+            var inventorAppType = Type.GetTypeFromProgID("Inventor.Application", true);
+            return (Application)Activator.CreateInstance(inventorAppType);
+        }
+    }
+}
---
+++ InventorTests/Tests.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace InventorTests
+{
+    public class Tests
+    {
+        [Fact]
+        public void CanGetInventorInstance()
+        {
+            var inventorApp = InventorAppUtils.GetInventorInstance();
+            Assert.NotNull(inventorApp);
+        }
+    }
+}
